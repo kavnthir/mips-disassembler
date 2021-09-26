d118950e2d2f3c532f263be7e28d0fb1209f076e
Kavin Thirukonda <kavnthir@vt.edu>
2021-09-25 21:06:53 -0400

    cmake and .gitignore

--- disassembler.cpp
+++ disassembler.cpp
@@ -1,18 +1,5 @@
 #include "instruction.hpp"
 
-int main(int argc, char* argv[]){
-	if(argc == 0){
-		std::cout << "Please specify *.obj";
-		return -1;
-	}else if(!disassemble(argv[1])){
-		std::cout << argv[1] << " not found.\n";
-		return -1;
-	}else{
-		std::cout << "disassembled code outputted to *.s\n";
-		return 0;
-	}
-}	
-
 bool disassemble(std::string filepath){
 	std::ifstream input;
 	std::string instruction;
@@ -24,8 +11,22 @@ bool disassemble(std::string filepath){
 			std::cout << instruction.getInstruction(AssemblyCode) << "\n";
 			// store assembly code to *.s
 		}
+		return true;
 	}else{
 		return false;
 	}
 	input.close();
 }
+
+int main(int argc, char* argv[]){
+	if(argc == 1){
+		std::cout << "please specify a *.obj file.";
+		return -1;
+	}else if(!disassemble(argv[1])){
+		std::cout << argv[1] << " not found.\n";
+		return -1;
+	}else{
+		std::cout << "disassembled code outputted to *.s\n";
+		return 0;
+	}
+}	
